06f824f57824f726bc86c61646d5d77de5cabfce
pTran24 <xajin@xajinMBP.local>
2014-09-13 16:13:47 -0700

    Added triangle class

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -43,3 +43,57 @@ class Square: NamedShape {
 var square = Square(sideLength: 7, name: "Sue")
 square.simpleDescription()
 square.calcArea()
+
+class Circle: NamedShape {
+    var radius: Double
+    let pi = 3.141592
+    
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+    }
+    
+    override func simpleDescription() -> String {
+        return "Cicle with radius of \(radius)."
+    }
+    
+    func calcArea() -> Double {
+        return pi * (radius * radius)
+    }
+}
+
+var circ = Circle(radius: 7, name: "Joe")
+circ.simpleDescription()
+circ.calcArea()
+
+class EquilateralTriangle: NamedShape{
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+    }
+    
+    var perimeter: Double {
+        get{
+            return 3.0 * sideLength
+        }
+        set{
+            //newValue is a special implicit variable in setters
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "AN equilateral stringle with sides of length \(sideLength)."
+    }
+}
+
+var triangle = EquilateralTriangle(sideLength: 100, name: "Jeni")
+triangle.perimeter
+triangle.sideLength
+triangle.perimeter = 27
+triangle.sideLength
+
+
+
